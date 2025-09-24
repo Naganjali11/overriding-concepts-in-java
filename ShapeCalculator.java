d325fa4b8526ef89c76e86ecb80322887f18ee45
Naganjali11 <naganjali425@gmail.com>
2025-09-24 16:09:15 +0530

    javafullstackprograms

---
+++ ShapeCalculator.java
@@ -0,0 +1,15 @@
+package com.codegnan.oop.overriding;
+
+public class ShapeCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cube cubeInstance=new Cube();
+		Cuboid cuboidInstance=new Cuboid();
+		 cubeInstance.calculateVolume(); 
+	     cuboidInstance.calculateVolume();
+		
+
+	}
+
+}
