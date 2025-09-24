d91fc69afc3e5ec80a74e74cae87023dbdf82494
Naganjali11 <naganjali425@gmail.com>
2025-09-24 16:07:26 +0530

    javafullstackprograms

---
+++ Parent.java
@@ -0,0 +1,12 @@
+package com.codegnan.oop.overriding;
+
+public class Parent {
+	public void property() {
+		System.out.println("Cash/Land/Gold");
+	}
+	//overridden method
+	public void marry() {
+		System.out.println("Subbalakshmi");
+	}
+
+}
