eec79d2fb14d6d5a8048800243a7699f58817677
Naganjali11 <naganjali425@gmail.com>
2025-09-24 16:09:35 +0530

    javafullstackprograms

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+package com.codegnan.oop.overriding;
+
+public class Triangle extends Figure{
+	public Triangle(double dim1,double dim2) {
+		super(dim1,dim2);
+	}
+	public double area() {
+		System.out.println("Inside Area for Triangle");
+		return(dim1*dim2)/2;
+	}
+
+}
